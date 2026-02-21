f05d45addc1f6aa7c403a18bbf945417b5c1c22d
Ouroboros <ouroboros@users.noreply.github.com>
2026-02-21 06:04:16 +0400

    test: list chats

---
+++ tg_list_chats_test.py
@@ -0,0 +1,14 @@
+import asyncio
+import os
+os.environ['TELEGRAM_SESSION_PATH'] = '/home/a/ouroboros_repo/session.session'
+from telethon import TelegramClient
+
+async def main():
+    client = TelegramClient(os.environ['TELEGRAM_SESSION_PATH'], 24553863, '9364ce0067c945a18425912fb9ab92bb')
+    await client.start()
+    print("Chats I'm in:")
+    async for dialog in client.iter_dialogs():
+        print(f"  {dialog.entity.title}: @{dialog.entity.username or ''}")
+    await client.disconnect()
+
+asyncio.run(main())
